3b35b2bfd61df14a833188460e2d3a3b1e6745a6
LucyWangMin <37284301+LucyWangMin@users.noreply.github.com>
2019-04-02 17:04:23 +0800

    Create b.php

---
+++ b.php
@@ -0,0 +1,12 @@
+<?php
+ 
+$callback = !empty($_GET['callback']) ? trim($_GET['callback']) : '';
+ 
+if(!empty($callback)) {
+    $data = json_encode(array(
+        'uid' => 1,
+        'name' => '测试',
+    ));
+     
+    echo "{$callback}({$data});";
+}
